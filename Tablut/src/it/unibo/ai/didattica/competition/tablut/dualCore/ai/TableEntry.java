864b46d775b42596f88af23c1ee55b5544caf5eb
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2021-11-23 00:10:41 +0100

    fix

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/ai/TableEntry.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/ai/TableEntry.java
@@ -1,11 +1,18 @@
 package it.unibo.ai.didattica.competition.tablut.dualCore.ai;
 
 public class TableEntry {
+
+    public enum FlagValue {
+        EXACT,
+        LOWER_BOUND,
+        UPPER_BOUND,
+    }
+
     Float value;
-    String flag;
+    FlagValue flag;
     Integer depth;
     
-    public TableEntry(Float value, String flag, Integer depth) {
+    public TableEntry(Float value, FlagValue flag, Integer depth) {
         this.value = value;
         this.flag = flag;
         this.depth = depth;
