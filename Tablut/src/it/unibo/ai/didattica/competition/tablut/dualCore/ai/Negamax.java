bb83e82fec2df95475c480b0cdda09b26b45d943
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2021-11-22 22:41:01 +0100

    Negamax started

---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/ai/Negamax.java
@@ -0,0 +1,26 @@
+package it.unibo.ai.didattica.competition.tablut.dualCore.ai;
+
+import it.unibo.ai.didattica.competition.tablut.domain.Action;
+import it.unibo.ai.didattica.competition.tablut.domain.State;
+
+import java.util.ArrayList;
+
+public final class Negamax {
+    private static final int TABLE_SIZE = 1_000_000;
+
+    private final int color;
+    private final int timeout;
+    private final HeuristicsTable heuristicsTable;
+    private final TraspositionTable traspositionTable;
+
+    public Negamax(int color, int timeout) {
+        this.color = color;
+        this.timeout = timeout - 1;
+        traspositionTable = new TraspositionTable(TABLE_SIZE);
+        heuristicsTable = new HeuristicsTable(TABLE_SIZE);
+    }
+
+    public final Action findBestMove(State state, ArrayList<Integer> previousStates) {
+
+    }
+}
--- Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/game/Player.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/game/Player.java
@@ -2,6 +2,7 @@ package it.unibo.ai.didattica.competition.tablut.dualCore.game;
 
 import it.unibo.ai.didattica.competition.tablut.domain.Action;
 import it.unibo.ai.didattica.competition.tablut.domain.State;
+import it.unibo.ai.didattica.competition.tablut.dualCore.ai.Negamax;
 import it.unibo.ai.didattica.competition.tablut.dualCore.utils.PlayerRoleMapper;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,26 +10,20 @@ import java.util.ArrayList;
 public class Player {
     private final ArrayList<Integer> previousStates = new ArrayList<>();
     private final State.Turn turn;
-    private final int color;
-    // private final Negamax
+    // private final int color;
+    // private final int timeout;
+    private final Negamax negamax;
 
     // Todo initialize negamax
-    public Player(State.Turn turn) {
+    public Player(State.Turn turn, int timeout) {
         this.turn = turn;
-        this.color = PlayerRoleMapper.getColor(turn);
+        int color = PlayerRoleMapper.getColor(turn);
+        negamax = new Negamax(color, timeout);
     }
 
-    // Todo: implement negamax return value
-    public final Action chooseMove(State stateType) {
-        // bestMove = self.negamax.find_best_move(Node(state))
-        // return MoveHasher.format_move(best_move)
-        Action action = null;
-        try {
-            action = new Action("a4", "a5", turn);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return action;
+    // Todo: implement negamax return value pass to findBestMove
+    public final Action chooseMove(State state) {
+        return negamax.findBestMove(state, previousStates);
     }
 
     public final void saveState(State state) {
