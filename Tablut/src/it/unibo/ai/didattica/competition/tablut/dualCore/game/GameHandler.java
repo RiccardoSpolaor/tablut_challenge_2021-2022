6591000bbb8e1a421d4b350be5bd96e61e873f88
Facundo Nicolas Maidana <f.maidana@iconsulting.biz>
2021-11-23 08:08:18 +0100

    camp references added

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/game/GameHandler.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/dualCore/game/GameHandler.java
@@ -9,6 +9,28 @@ import it.unibo.ai.didattica.competition.tablut.domain.State.Pawn;
 
 public class GameHandler {
 
+    private final int[][] camps = {
+        {0,3}, {0,4}, {0,5}, {1,4},
+        {3,0}, {3,8}, {4,0}, {4,1},
+        {4,7}, {4,8}, {5,0}, {5,8},
+        {7,4}, {8,3}, {8,4}, {8,5}
+    };
+
+    private final int[][] escapes = {
+        {0,1}, {0,2}, {0,6}, {0,7},
+        {1,0}, {1,8}, {2,0}, {2,8},
+        {6,0}, {6,8}, {7,0}, {7,8},
+        {8,1}, {8,2}, {8,6}, {8,7}
+    };
+
+    private final int[][] escape_block = {
+        {1,2}, {1,6}, {2,1}, {2,2}, {2,6}, {2,7},
+        {6,1}, {6,7}, {7,2}, {7,6}, {6,2}, {6,6}
+    };
+
+    private final Integer boardLenght = 9;
+    private final int[][] throne = {{4,4}};
+
     private final static ArrayList<Integer> previousStates = new ArrayList<>();
 
     public static ArrayList<Integer> getPreviousStates() {
@@ -18,8 +40,6 @@ public class GameHandler {
     public static void updatePreviousStates(State state) {
        previousStates.add(state.hashCode());
     }
-
-    private final int[][] camps;
 
     public static Float getKingOpenPathToGoal(Pawn[][] board) {
         return null;
